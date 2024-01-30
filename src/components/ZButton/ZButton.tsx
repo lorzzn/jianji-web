25f2a72740f2b3acafb17b4f4fcfaa24ea60aefb
ppengryuu <255499213@qq.com>
2024-01-30 23:48:00 +0800

    update: 封装按钮组件
    reason: 无

---
+++ src/components/ZButton/ZButton.tsx
@@ -0,0 +1,48 @@
+import { cva, type VariantProps } from "class-variance-authority"
+import classNames from "classnames"
+import { FC } from "react"
+import { twMerge } from "tailwind-merge"
+
+interface ZButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {}
+
+const buttonVariants = cva(
+  classNames('inline-flex items-center justify-center disabled:opacity-50'),
+  {
+    variants: {
+      variant: {
+        primary: classNames('bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700 transition'),
+        success: classNames('bg-green-500 text-white hover:bg-green-600 active:bg-green-700 transition'),
+        danger: classNames('bg-red-500 text-white hover:bg-red-600 active:bg-red-700 transition'),
+      },
+      scale: {
+        small: classNames('h-6 px-3 text-xs'),
+        middle: classNames('h-8 px-3 text-sm'),
+        large: classNames('h-10 px-5 text-[1rem]'),
+      },
+      shape: {
+        round: classNames('rounded-full'),
+        square: classNames('rounded-s rounded-e'),
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      scale: 'middle',
+      shape: "square",
+    },
+  }
+)
+
+const ZButton:FC<ZButtonProps> = ({ className, variant, scale, shape, children, ...restProps }) => {
+  return (
+    <button
+      className={twMerge(
+        classNames(buttonVariants({ variant, scale, shape, className }))
+      )}
+      {...restProps}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default ZButton
