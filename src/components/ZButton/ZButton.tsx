882ed1300b80deac68c36ed4559a2342fd2057c2
lorenzozane <255499213@qq.com>
2024-02-27 16:40:20 +0800

    update: 按钮组件支持显示loading
    reason: 无

--- src/components/ZButton/ZButton.tsx
+++ src/components/ZButton/ZButton.tsx
@@ -1,9 +1,13 @@
+import { RiLoader4Fill } from "@remixicon/react"
 import { cva, type VariantProps } from "class-variance-authority"
 import classNames from "classnames"
 import { FC } from "react"
 import { twMerge } from "tailwind-merge"
+import { motion } from 'framer-motion'
 
-interface ZButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {}
+interface ZButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+  loading?: boolean
+}
 
 const buttonVariants = cva(
   classNames('inline-flex items-center justify-center disabled:opacity-50'),
@@ -32,14 +36,23 @@ const buttonVariants = cva(
   }
 )
 
-const ZButton:FC<ZButtonProps> = ({ className, variant, scale, shape, children, ...restProps }) => {
+const ZButton:FC<ZButtonProps> = ({ className, variant, scale, shape, children, loading, disabled, ...restProps }) => {
   return (
     <button
       className={twMerge(
-        classNames(buttonVariants({ variant, scale, shape, className }))
+        classNames([buttonVariants({ variant, scale, shape, className }), { "cursor-not-allowed": loading || disabled }])
       )}
+      disabled={loading || disabled}
       {...restProps}
     >
+      {loading && <motion.div
+        className="mr-1"
+        initial={{ rotate: '0deg' }}
+        animate={{ rotate: '360deg' }}
+        transition={{ duration: 2, repeat: Infinity, ease: "linear" }}
+      >
+        <RiLoader4Fill />
+      </motion.div>}
       {children}
     </button>
   )
