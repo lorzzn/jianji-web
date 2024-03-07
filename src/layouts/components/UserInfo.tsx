803470a5893165b99c5e07dbf7d36eb3bfcc0578
lorenzozane <255499213@qq.com>
2024-03-07 23:55:48 +0800

    update: 修改昵称最大宽度
    reason: 无

--- src/layouts/components/UserInfo.tsx
+++ src/layouts/components/UserInfo.tsx
@@ -24,7 +24,7 @@ const UserInfo:FC = () => {
       userInfo.id ? <button onClick={onUserClick} title={userInfo.name}>
         <div className='flex justify-center items-center h-full space-x-2'>
           <ZImage src={userInfo.avatar} className='w-8 h-8 rounded-full' />
-          <span className='text-sm max-w-36 overflow-hidden overflow-ellipsis'>{userInfo.name}</span>
+          <span className='text-sm max-w-28 overflow-hidden overflow-ellipsis'>{userInfo.name}</span>
         </div>
       </button> : <button onClick={onLoginClick} title="登录/注册">
         <span>登录/注册</span>
