5a3f099849fc48a19990f7786783594ab5cab619
lorzzn <255499213@qq.com>
2024-03-09 01:02:08 +0800

    update: 添加公共组件ZDropdown
    reason: 无

---
+++ src/components/ZDropdown/ZDropdown.tsx
@@ -0,0 +1,94 @@
+import { CSSProperties, ForwardRefRenderFunction, ReactNode, forwardRef, useImperativeHandle, useRef, useState } from "react";
+import Select, { ClassNamesConfig, SelectInstance, SingleValue, StylesConfig } from 'react-select';
+import classNames from "classnames";
+import { omit } from 'lodash';
+import { twMerge } from "tailwind-merge";
+
+export interface DropdownOption {
+  readonly value: string;
+  readonly label: string;
+  readonly style?: CSSProperties
+  readonly selectable?: boolean
+}
+
+interface ZDropdownProps {
+  target?: ReactNode
+  options: DropdownOption[]
+  onChange?: (option: DropdownOption) => void
+  onClick?: (type: "target" | "option", option?: DropdownOption) => void
+  classNames?: ClassNamesConfig<DropdownOption, false>
+}
+
+const selectStyles:StylesConfig<DropdownOption, false> = {
+  control: () => ({ display: "none" }),
+  menu: () => ({}),
+  option: (base, props) => {
+    return {
+      ...base,
+      ...props.data.style
+    }
+  },
+}
+
+export interface ZDropdownRef {
+  open: Function
+  close: Function
+}
+
+const ZDropdown:ForwardRefRenderFunction<ZDropdownRef, ZDropdownProps> = ({ target, options, onChange, onClick, classNames: propClassNames }, ref) => {
+ 
+  const [open, setOpen] = useState(false)
+  const [value, setValue] = useState<DropdownOption | null>(null)
+  const selectRef = useRef<SelectInstance<DropdownOption> | null>(null)
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+  const onOpen = () => setOpen(true)
+  const onClose = () => setOpen(false)
+  const onButtonClick = () => {
+    (open ? onClose:onOpen)()
+    onClick?.("target")
+  }
+
+  const onSelectChange = (item: SingleValue<DropdownOption>) => {
+    if (item?.selectable) {
+      setValue(item)
+      onChange?.(item)
+    }
+    if (item) {
+      onClick?.("option", item)
+    }
+    onClose()
+  }
+
+  const selectClassNames:ClassNamesConfig<DropdownOption, false> = {
+    menu: (props) => twMerge(classNames(['bg-white rounded-md shadow-md', propClassNames?.menu?.(props)])),
+    menuList: (props) => twMerge(classNames(['px-[4px]', propClassNames?.menuList?.(props)])),
+    option: (props) => twMerge(classNames(['text-center rounded-md hover:text-blue-600 !text-sm !cursor-pointer', propClassNames?.option?.(props)])),
+    ...omit(propClassNames, "menu", "menuList", "option")
+  }
+
+  useImperativeHandle(ref, () => ({
+    close: onClose,
+    open: onOpen
+  }))
+
+  return <div className="relative">
+    <button ref={buttonRef} onClick={onButtonClick}>{target}</button>
+    {
+      open && <div className="absolute" >
+        <button className="fixed inset-0 cursor-default" onClick={onClose}></button>
+        <Select
+          ref={selectRef}
+          styles={selectStyles}
+          classNames={selectClassNames}
+          menuIsOpen
+          options={options}
+          onChange={onSelectChange}
+          value={value}
+        />
+      </div>
+    }
+  </div>
+
+}
+
+export default forwardRef(ZDropdown)
