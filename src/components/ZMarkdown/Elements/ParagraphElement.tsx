8d2038e4afcf3bbe667272929f57b8466674e620
lorzzn <255499213@qq.com>
2024-03-18 06:56:27 +0800

    update: 删掉多余组件
    reason: 无

--- src/components/ZMarkdown/Elements/ParagraphElement.tsx
+++
@@ -1,7 +0,0 @@
-import { CustomElementProps, ParagraphElementType } from "../customTypes"
-
-const ParagraphElement = (props: CustomElementProps<ParagraphElementType>) => {
-  return <div {...props.attributes}>{props.children}</div>
-}
-
-export default ParagraphElement
