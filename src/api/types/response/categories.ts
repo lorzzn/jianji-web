bd4d33fde47f4686a06b11b9662d3a42d438a434
lorzzn <255499213@qq.com>
2024-04-03 11:16:50 +0800

    update: 分类支持排序
    reason: 无

--- src/api/types/response/categories.ts
+++ src/api/types/response/categories.ts
@@ -3,8 +3,8 @@ import { IApiCommonResponse } from "./common"
 export interface ICategories {
   label: string
   value: number
-  parentValue: number
-  path: string
+  parentValue: number | null
+  ordinalNumber: number
 }
 
 export interface ICategoriesResponse extends IApiCommonResponse {
