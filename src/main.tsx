7a8c18d5c578db0628f1056994b8828f32f20ff2
lorenzozane <255499213@qq.com>
2024-04-04 01:18:43 +0800

    update: 分类选择添加右键菜单
    reason: 无

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import App from "@/App.tsx"
 import "@/index.css"
 import "rc-tree/assets/index.css"
+import "react-contexify/ReactContexify.css"
 import ReactDOM from "react-dom/client"
 import "react-responsive-modal/styles.css"
 import "url-search-params-polyfill"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />)
+export const reactRootElement = document.getElementById("root")!
+
+ReactDOM.createRoot(reactRootElement).render(<App />)
