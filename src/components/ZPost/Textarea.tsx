528bd22cc43b9d1ae93a9ee09d0c52b095379da7
lorzzn <255499213@qq.com>
2024-04-12 21:55:45 +0800

    update: 编辑器快捷键
    reason: 无

--- src/components/ZPost/Textarea.tsx
+++ src/components/ZPost/Textarea.tsx
@@ -1,12 +1,45 @@
 import { twclx } from "@/utils/twclx"
-import { FC } from "react"
+import { assign } from "lodash"
+import { forwardRef, useImperativeHandle, useRef } from "react"
 
 interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   resizeable?: boolean
 }
 
-const Textarea: FC<TextareaProps> = ({ resizeable, className, ...restProps }) => {
-  return <textarea className={twclx([{ "resize-none": !resizeable }, className])} {...restProps} />
+export interface TextareaRef extends HTMLTextAreaElement {
+  getCursorPostion: () => { line: number; column: number }
+  getLines: () => string[]
 }
+
+const Textarea = forwardRef<TextareaRef, TextareaProps>(({ resizeable, className, ...restProps }, ref) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+  const getCursorPostion = () => {
+    const textarea = textareaRef.current
+    if (!textarea) return { line: -1, column: -1 }
+    const { selectionStart, value } = textarea
+
+    const lines = value.substring(0, selectionStart).split("\n")
+    const line = lines.length - 1
+    const column = lines[line].length
+
+    return { line, column }
+  }
+
+  const getLines = () => {
+    const textarea = textareaRef.current
+    if (!textarea) return []
+    const { value } = textarea
+    return value.split("\n")
+  }
+
+  useImperativeHandle(ref, () =>
+    assign(textareaRef.current, {
+      getCursorPostion,
+      getLines,
+    }),
+  )
+
+  return <textarea ref={textareaRef} className={twclx([{ "resize-none": !resizeable }, className])} {...restProps} />
+})
 
 export default Textarea
