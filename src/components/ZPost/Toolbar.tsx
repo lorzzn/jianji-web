3a3b626f098953b0e8af0daff76111f447b6d513
lorzzn <255499213@qq.com>
2024-04-12 11:02:22 +0800

    update: 编辑器工具栏
    reason: 无

--- src/components/ZPost/Toolbar.tsx
+++ src/components/ZPost/Toolbar.tsx
@@ -1,13 +1,81 @@
-import { FC } from "react"
+import { twclx } from "@/utils/twclx"
+import { RiBallPenLine, RiEyeLine, RiFullscreenExitLine, RiFullscreenLine, RiLayoutColumnFill } from "@remixicon/react"
+import classNames from "classnames"
+import { FC, useMemo } from "react"
 
-const Toolbar: FC = () => {
-  const toolBtns = [
-    {
-      // icon
-    },
-  ]
+export interface ToolbarButton {
+  title: string
+  icon?: JSX.Element
+  active?: boolean
+  layout?: string
+  className?: string
+  action?: string
+}
 
-  return <div></div>
+interface ToolbarProps {
+  layout: string
+  fullscreen: boolean
+  onClick: (info: ToolbarButton, event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const Toolbar: FC<ToolbarProps> = ({ layout, fullscreen, onClick }) => {
+  const buttons = useMemo<ToolbarButton[]>(() => {
+    return [
+      {
+        icon: <RiLayoutColumnFill size={"1.1rem"} className={classNames([{ "rotate-z-180": layout === "reverse" }])} />,
+        title: "布局",
+        active: layout === "normal" || layout === "reverse",
+        layout: layout === "normal" ? "reverse" : "normal",
+      },
+      {
+        icon: <RiBallPenLine size={"1.1rem"} />,
+        title: "编辑器",
+        active: layout === "editor",
+        layout: "editor",
+      },
+      {
+        icon: <RiEyeLine size={"1.1rem"} />,
+        title: "预览",
+        active: layout === "preview",
+        layout: "preview",
+      },
+      {
+        className: "ml-auto",
+        title: "帮助",
+        action: "help",
+      },
+      {
+        icon: fullscreen ? <RiFullscreenExitLine size={"1.1rem"} /> : <RiFullscreenLine size={"1.1rem"} />,
+        title: "全屏",
+        action: "fullscreen",
+      },
+    ]
+  }, [layout, fullscreen])
+
+  const onButtonClick = (info: ToolbarButton, event: React.MouseEvent<HTMLButtonElement>) => {
+    onClick?.(info, event)
+  }
+
+  return (
+    <div className="flex w-full">
+      {buttons.map((button, index) => {
+        return (
+          <button
+            key={index}
+            className={twclx([
+              "flex justify-center items-center px-3 py-2 space-x-1 hover:bg-gray-200",
+              { "bg-gray-200": button.active },
+              button.className,
+            ])}
+            onClick={(e) => onButtonClick(button, e)}
+          >
+            {button.icon}
+            <span>{button.title}</span>
+          </button>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Toolbar
