4c57b83ddfb8ee5771e15018035faef83d3e867e
lorenzozane <255499213@qq.com>
2024-04-12 11:02:22 +0800

    update: 编辑器工具栏
    reason: 无

--- src/components/ZPost/ZPostEditor.tsx
+++ src/components/ZPost/ZPostEditor.tsx
@@ -1,40 +1,61 @@
+import classNames from "classnames"
 import { FC, useState } from "react"
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
 import Preview from "./Preview"
 import Textarea from "./Textarea"
+import Toolbar, { ToolbarButton } from "./Toolbar"
 
 const ZPostEditor: FC = () => {
   const [value, setValue] = useState<string>("")
+  const [layout, setLayout] = useState<string>("normal")
+  const [fullscreen, setFullscreen] = useState(false)
 
   const handleValueChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(e.target.value)
   }
 
+  const onToolbarClick = (info: ToolbarButton) => {
+    if (info.layout) {
+      setLayout(info.layout)
+    }
+
+    if (info.action === "fullscreen") {
+      setFullscreen(!fullscreen)
+    }
+
+    if (info.action === "help") {
+      console.log("help")
+    }
+  }
+
   return (
-    <div className="flex-1 flex flex-col">
-      <div>sasasas</div>
-      <PanelGroup direction="horizontal" className="flex-1">
-        <Panel minSize={25} order={1} className="shadow-sm rounded-md">
-          <Textarea
-            placeholder="请输入markdown内容..."
-            value={value}
-            onChange={handleValueChange}
-            className="w-full h-full p-3"
-          />
-        </Panel>
-        <PanelResizeHandle className="w-2 flex justify-center items-center active:bg-black hover:bg-black transition rounded-sm">
-          <div className="flex flex-col space-y-1">
-            <div className="w-1 h-1 bg-white rounded-full"></div>
-            <div className="w-1 h-1 bg-white rounded-full"></div>
-            <div className="w-1 h-1 bg-white rounded-full"></div>
-          </div>
-        </PanelResizeHandle>
-        <Panel minSize={25} order={2} className="shadow-sm rounded-md">
-          <div className="w-full h-full bg-white p-3">
-            <Preview>{value}</Preview>
-          </div>
-        </Panel>
-      </PanelGroup>
+    <div className={classNames(["flex-1 flex flex-col border bg-gray-100", { "fixed inset-0 z-[500]": fullscreen }])}>
+      <Toolbar layout={layout} fullscreen={fullscreen} onClick={onToolbarClick} />
+      <div className={"flex-1 flex flex-col"}>
+        <PanelGroup
+          direction="horizontal"
+          className={classNames(["flex-1", { "!flex-row-reverse": layout === "reverse" }])}
+        >
+          {layout !== "preview" && (
+            <Panel minSize={25} id="editor-panel" order={1}>
+              <Textarea
+                placeholder="请输入markdown内容..."
+                value={value}
+                onChange={handleValueChange}
+                className="w-full h-full p-3"
+              />
+            </Panel>
+          )}
+          {!["editor", "preview"].includes(layout) && <PanelResizeHandle className="w-0 border" />}
+          {layout !== "editor" && (
+            <Panel minSize={25} id="preview-panel" order={2}>
+              <div className="w-full h-full bg-white p-3">
+                <Preview>{value}</Preview>
+              </div>
+            </Panel>
+          )}
+        </PanelGroup>
+      </div>
     </div>
   )
 }
