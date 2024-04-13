f774423a8af89b3d1e6f4a0fbfd24af6a26ba9e6
lorenzozane <255499213@qq.com>
2024-04-13 16:06:14 +0800

    update: 添加公共组件
    reason: 无

---
+++ src/components/ZList/ZList.tsx
@@ -0,0 +1,25 @@
+import { twclx } from "@/utils/twclx";
+import { css } from "@emotion/css";
+import { FC } from "react";
+import tw from "twin.macro";
+
+interface ZListProps extends React.InputHTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+}
+
+const ZList :FC<ZListProps> = (props) => {
+
+  return <div className={twclx([
+    "z-list flex flex-col",
+    css`
+      .z-list-item:nth-child(even){
+        ${tw`bg-gray-100`}
+      }
+    `,
+    props.className
+  ])}>
+    {props.children}
+  </div>
+}
+
+export default ZList
