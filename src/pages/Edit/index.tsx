3703521998b88f4d285cc16b6e620a4d3e8e342c
lorenzozane <255499213@qq.com>
2024-04-14 13:07:36 +0800

    update: code, add uuid
    reason: 无

--- src/pages/Edit/index.tsx
+++ src/pages/Edit/index.tsx
@@ -3,6 +3,7 @@ import ZInput, { ZInputProps } from "@/components/ZInput/ZInput"
 import ZPostEditor from "@/components/ZPost/ZPostEditor"
 import useDialog, { dialogNames } from "@/hooks/useDialog"
 import { useStore } from "@/store"
+import { uuidjs } from "@/utils/uuid"
 import { observer } from "mobx-react"
 import { FC, useEffect } from "react"
 import { useParams } from "react-router-dom"
@@ -17,14 +18,16 @@ const Edit: FC = observer(() => {
 
   const { dialog } = useDialog(dialogNames.SavePostDialog)
   const showSaveDialog = () => dialog()?.show()
-  
-  const { uuid } = useParams()
+
+  const { uuid: uuidparam } = useParams()
 
   useEffect(() => {
-    if (uuid) {
-      getFromRemote(uuid)
+    let uuid = uuidjs.NIL
+    if (uuidparam && uuidjs.validate(uuidparam)) {
+      uuid = uuidparam
     }
-  }, [uuid])
+    getFromRemote(uuid)
+  }, [ uuidparam ])
 
   const onTitleChange:ZInputProps["onChange"] = (e) => {
     setTitle(e.target.value)
