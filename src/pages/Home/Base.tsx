968185850c54c5d6acb92d701eefe14702ed9db9
lorenzozane <255499213@qq.com>
2024-04-15 17:01:07 +0800

    update: code
    reason: 无

--- src/pages/Home/Base.tsx
+++ src/pages/Home/Base.tsx
@@ -2,21 +2,31 @@ import { IListPostsRequest } from "@/api/types/request/posts"
 import ZPagination from "@/components/ZPagination/ZPagination"
 import ZPostCard from "@/components/ZPostCard/ZPostCard"
 import usePostList from "@/hooks/usePostList"
+import eventBus from "@/utils/eventBus"
+import { updateQueryString } from "@/utils/queryString"
+import queryString from "query-string"
 import { FC, useEffect } from "react"
+import { useLocation } from "react-router-dom"
 
 interface BaseProps {
   extraParams?: Partial<IListPostsRequest>
 }
 
 const Base: FC<BaseProps> = ({ extraParams }) => {
+  const location = useLocation()
+  const queryParams = queryString.parse(location.search)
+  const pageNo = Number(queryParams.page || 1)
 
-  const { list, getList, pageInfo, totalPage, updatePageNo } = usePostList({
-    pageNo: 1,
-    pageSize: 10,
-  }, {
-    archived: false,
-    ...extraParams
-  })
+  const { list, getList, pageInfo, totalPage, updatePageNo } = usePostList(
+    {
+      pageNo,
+      pageSize: 10,
+    },
+    {
+      archived: false,
+      ...extraParams,
+    },
+  )
 
   useEffect(() => {
     getList()
@@ -24,20 +34,22 @@ const Base: FC<BaseProps> = ({ extraParams }) => {
 
   const onPageChange = (e: number) => {
     updatePageNo(e)
+    updateQueryString("page", e)
+    eventBus.emit("scrolltotop")
   }
 
   return (
     <>
       <div className="flex flex-col justify-center items-center mt-12 flex-1">
-        {
-          list.map(item => <ZPostCard post={item} key={item.uuid} />)
-        }
+        {list.map((item) => (
+          <ZPostCard post={item} key={item.uuid} />
+        ))}
       </div>
 
       {/* 分页 */}
       <div className="flex flex-col justify-center items-center my-6">
         <div className="w-9/12 max-w-7xl">
-          <ZPagination currentPage={pageInfo.pageNo} totalPage={totalPage} jumpSize={5} onPageChange={onPageChange}/>
+          <ZPagination currentPage={pageInfo.pageNo} totalPage={totalPage} jumpSize={5} onPageChange={onPageChange} />
         </div>
       </div>
     </>
