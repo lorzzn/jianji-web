4b0698cd760382d0d13a6aafdb6549868905665d
lorzzn <255499213@qq.com>
2024-04-17 16:37:12 +0800

    update: 优化权限验证
    reason: 无

--- src/layouts/components/RequireAuth.tsx
+++ src/layouts/components/RequireAuth.tsx
@@ -1,7 +1,7 @@
 import { useStore } from "@/store"
 import { observer } from "mobx-react"
-import { FC } from "react"
-import { Navigate } from "react-router-dom"
+import { FC, useEffect } from "react"
+import { Navigate, useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
 
 interface RequireAuthProps {
@@ -11,13 +11,17 @@ interface RequireAuthProps {
 const RequireAuth: FC<RequireAuthProps> = observer(({ children }) => {
   const { userStore } = useStore()
   const { authed, loading } = userStore
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!authed) {
+      toast.info("请登录账号")
+      navigate("/login")
+    }
+  }, [])
 
   if (loading) {
     return <></>
-  }
-
-  if (!authed) {
-    toast.info("请登录账号")
   }
 
   return authed ? children : <Navigate replace to={"/"} />
