43695dea950d0db29bfb5cb5375afdcf4167fb30
lorenzozane <255499213@qq.com>
2024-04-17 18:57:17 +0800

    update: 分类和标签页无数据效果
    reason:

--- src/pages/Tags/index.tsx
+++ src/pages/Tags/index.tsx
@@ -1,17 +1,23 @@
+import ZEmpty from "@/components/ZEmpty/ZEmpty"
 import Tag from "@/components/ZPost/Tag"
 import { useStore } from "@/store"
+import { isEmpty } from "lodash"
 import { observer } from "mobx-react"
 import { FC, useEffect } from "react"
 
 const Tags: FC = observer(() => {
   const { tagsStore } = useStore()
-  const { tags, getTags } = tagsStore
+  const { tags, getTags, loading } = tagsStore
 
   useEffect(() => {
     getTags()
   }, [])
 
-  return (
+  return isEmpty(tags) && !loading ? (
+    <div className="flex-1 flex place-content-center">
+      <ZEmpty />
+    </div>
+  ) : (
     <>
       <div className="flex justify-center mt-24">
         <div className="flex flex-wrap w-7/12 max-w-5xl -mr-5 -mb-4">
